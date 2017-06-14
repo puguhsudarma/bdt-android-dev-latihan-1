e1b2a15ef7866a09134c13077289a0e5ca92b43d
Reroet <wayanpuguhsudarma@gmail.com>
2017-06-14 20:40:21 +0800

    checkpoint belajar intent dashboard activity

---
+++ app/src/main/java/com/reroet/bdtbatch2_latihan/DashboardActivity.java
@@ -0,0 +1,17 @@
+package com.reroet.bdtbatch2_latihan;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by reroet on 14/06/17.
+ */
+
+public class DashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- app/src/main/java/com/reroet/bdtbatch2_latihan/MainActivity.java
+++ app/src/main/java/com/reroet/bdtbatch2_latihan/MainActivity.java
@@ -1,13 +1,48 @@
 package com.reroet.bdtbatch2_latihan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Lifecycle", "OnCreate()");
+
+        login = (Button) findViewById(R.id.btnLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+                startActivity(dashboard);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Lifecycle", "OnStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Lifecycle", "OnResume()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Lifecycle", "onDestroy()");
     }
 }
